56ef44931e91bfaadf4cc86ea640b8a553ec02b0
David Ovalle <davidovalle@habi.co>
2022-02-09 20:55:19 -0500

    using mypy to fix annotations

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -4,6 +4,7 @@ from typing import List, Tuple
 
 
 def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    """zooms an array to factor"""
     zoomed_in: List = [
         item for item in lst
         for i in range(factor)
