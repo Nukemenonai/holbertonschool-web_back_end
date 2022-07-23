2d464412cab4f26a938e48990413aa34df5d37e2
David Ovalle <davidovalle@habi.co>
2022-07-23 14:29:25 -0500

    adding docs

--- 0x03-caching/0-basic_cache.py
+++ 0x03-caching/0-basic_cache.py
@@ -8,14 +8,18 @@ class BasicCache(BaseCaching):
     '''
     Basic caching system
     '''
+    MAX_ITEMS = None
 
     def put(self, key, item):
+        """puts a value in self cache"""
         if not key or not item:
             pass
         else:
             self.cache_data[key] = item
 
     def get(self, key):
+        """returns value in self cache"""
         if not key:
             return None
-        return self.cache_data.get(key)
+        else:
+            return self.cache_data.get(key)
