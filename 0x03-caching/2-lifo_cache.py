ef4710a29459aa4e494482f75fc250ee4c9ee490
David Ovalle <david.giovanni.ovalle@gmail.com>
2023-04-08 14:17:56 -0500

    add: method to add key to last index of cache order

--- 0x03-caching/2-lifo_cache.py
+++ 0x03-caching/2-lifo_cache.py
@@ -19,18 +19,28 @@ class LIFOCache(BaseCaching):
         > than the max number of items
         '''
         if not key or not item:
-            pass
-        else:
-            if len(self.cache_data.values()) > BaseCaching.MAX_ITEMS:
+            return 
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            if self.cache_order:
                 last = self.cache_order.pop()
                 del self.cache_data[last]
-                print(f"DISCARD: {last}")
-            self.cache_data[key] = item
+                print('DISCARD: {}'.format(last))
+        
+        if key not in self.cache_order:
             self.cache_order.append(key)
+        else:
+            self.add_to_cache_order(key)
 
     def get(self, key):
         """get an item from cache by key """
-        if not key:
+        if not key or key not in self.cache_data:
             return None
         else:
             return self.cache_data.get(key)
+
+    def add_to_cache_order(self, key):
+        length = len(self.cache_order)
+        if self.cache_order[length - 1] != key:
+            self.cache_order.remove(key)
+            self.cache_order.append(key)
