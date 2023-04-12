6a349e2e6c013efa038f7f17f6e12608c24ca960
David Ovalle <david.giovanni.ovalle@gmail.com>
2023-04-11 19:15:33 -0500

    Update: add a LRU Cache

---
+++ 0x03-caching/3-lru_cache.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" LRU Cache Class
+"""
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    caching system that implements LIFO algo
+    """
+    def __init__(self):
+        super().__init__()
+        self.cache_order = []
+
+    def put(self, key, item):
+        '''
+        Puts an item in the cache
+        discards the last item put if the size of the cache
+        than the max number of items
+        '''
+        if not key or not item:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            if self.cache_order:
+                lru = self.cache_order.pop(0)
+                del self.cache_data[lru]
+                print('DISCARD: {}'.format(lru))
+
+        if key not in self.cache_order:
+            self.cache_order.append(key)
+        else:
+            self.add_to_cache_order(key)
+
+    def get(self, key):
+        """get an item from cache by key """
+        if not key or key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data.get(key)
+
+    def add_to_cache_order(self, key):
+        """add key to cache order if not already in cache order"""
+        length = len(self.cache_order)
+        if self.cache_order[length - 1] != key:
+            self.cache_order.remove(key)
+            self.cache_order.append(key)
